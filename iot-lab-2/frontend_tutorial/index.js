8aa976a6d6bc926c1154d7fbbddc9dec903e57f5
jonjonleeeee <92841461+jonjonleeeee@users.noreply.github.com>
2024-03-03 17:31:27 -0600

    Update index.js

--- iot-lab-2/frontend_tutorial/index.js
+++ iot-lab-2/frontend_tutorial/index.js
@@ -1,16 +1,96 @@
 var server_port = 65432;
 var server_addr = "192.168.50.213";   // the IP address of your Raspberry PI
 
-function client(){
+
+
+function client_forward(){
     
     const net = require('net');
-    var input = document.getElementById("myName").value;
+    var input = document.getElementById("speed").value;
 
     const client = net.createConnection({ port: server_port, host: server_addr }, () => {
         // 'connect' listener.
         console.log('connected to server!');
         // send the message
-        client.write(`${input}\r\n`);
+        client.write(`f${input}\r\n`);
+    });
+    
+    // get the data from the server
+    client.on('data', (data) => {
+        document.getElementById("greet_from_server").innerHTML = data;
+        console.log(data.toString());
+        client.end();
+        client.destroy();
+    });
+
+    client.on('end', () => {
+        console.log('disconnected from server');
+    });
+
+
+}
+function client_backward(){
+    
+    const net = require('net');
+    var input = document.getElementById("speed").value;
+
+    const client = net.createConnection({ port: server_port, host: server_addr }, () => {
+        // 'connect' listener.
+        console.log('connected to server!');
+        // send the message
+        client.write(`f-${input}\r\n`);
+    });
+    
+    // get the data from the server
+    client.on('data', (data) => {
+        document.getElementById("greet_from_server").innerHTML = data;
+        console.log(data.toString());
+        client.end();
+        client.destroy();
+    });
+
+    client.on('end', () => {
+        console.log('disconnected from server');
+    });
+
+
+}
+function client_left(){
+    
+    const net = require('net');
+    var input = document.getElementById("speed").value;
+
+    const client = net.createConnection({ port: server_port, host: server_addr }, () => {
+        // 'connect' listener.
+        console.log('connected to server!');
+        // send the message
+        client.write(`l${input}\r\n`);
+    });
+    
+    // get the data from the server
+    client.on('data', (data) => {
+        document.getElementById("greet_from_server").innerHTML = data;
+        console.log(data.toString());
+        client.end();
+        client.destroy();
+    });
+
+    client.on('end', () => {
+        console.log('disconnected from server');
+    });
+
+
+}
+function client_right(){
+    
+    const net = require('net');
+    var input = document.getElementById("speed").value;
+
+    const client = net.createConnection({ port: server_port, host: server_addr }, () => {
+        // 'connect' listener.
+        console.log('connected to server!');
+        // send the message
+        client.write(`r${input}\r\n`);
     });
     
     // get the data from the server
@@ -28,14 +108,3 @@ function client(){
 
 }
 
-function greeting(){
-
-    // get the element from html
-    var name = document.getElementById("myName").value;
-    // update the content in html
-    document.getElementById("greet").innerHTML = "Hello " + name + " !";
-    // send the data to the server 
-    to_server(name);
-    client();
-
-}
